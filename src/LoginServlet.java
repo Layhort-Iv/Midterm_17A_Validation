faa946c11bcb34334bd752ae4c77ac0e1cb57579
Iv Layhort <ivlayhort18@kit.edu.kh>
2020-03-24 16:16:48 +0700

    First

---
+++ src/LoginServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if(username.equals(password))
+            response.sendRedirect("Success.jsp");
+
+        else
+            response.sendRedirect("Error.jsp");
+    }
+
+}
---
+++ web/Error.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h1>Error</h1>
+    Click here to login again <a href="index.jsp">Login</a>
+</body>
+</html>
